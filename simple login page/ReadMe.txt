044a776c6d1ee5c688dc88e12d7bada0e4d1243a
manubuhay <manubuhay@gmail.com>
2019-02-12 17:21:23 +0800

    login drill
    Added simple login page drill

---
+++ simple login page/ReadMe.txt
@@ -0,0 +1,6 @@
+For Linux Environments refer to following for dependencies:
+https://stackoverflow.com/questions/13825108/undefined-function-mysql-connect
+https://stackoverflow.com/questions/1559955/host-xxx-xx-xxx-xxx-is-not-allowed-to-connect-to-this-mysql-server
+
+Drill taken from:
+http://mrbool.com/how-to-create-a-login-page-with-php-and-mysql/28656
---
+++ simple login page/connectivity.php
@@ -0,0 +1,43 @@
+<?php
+define('DB_HOST', '10.1.2.106');
+define('DB_NAME', 'login');
+define('DB_USER','root');
+define('DB_PASSWORD','password');
+
+$con=mysql_connect(DB_HOST,DB_USER,DB_PASSWORD) or die("Failed to connect to MySQL: " . mysql_error()); 
+$db=mysql_select_db(DB_NAME,$con) or die("Failed to connect to MySQL: " . mysql_error());
+
+$ID = $_POST['user']; 
+$Password = $_POST['pass'];
+
+function SignIn(){ 
+session_start(); //starting the session for user profile page
+
+	if(!empty($_POST['user']))
+	{
+		$query = mysql_query("SELECT * FROM UserName where userName = '$_POST[user]' AND pass = '$_POST[pass]'") or die(mysql_error());
+		$row = mysql_fetch_array($query) or die(mysql_error());
+			if(!empty($row['userName']) AND !empty($row['pass']))
+				{
+					$_SESSION['userName'] = $row['pass'];
+					echo "SUCCESSFULLY LOGIN TO USER PROFILE PAGE...";
+				}
+			if(!empty($row['userName']) AND empty($row['pass']))
+				{
+					$_SESSION['userName'] = $row['pass'];
+					echo "Password is Empty!";
+				}
+			if(empty($row['userName']) AND !empty($row['pass']))
+				{
+					$_SESSION['userName'] = $row['pass'];
+					echo "Username is Empty!";
+				}
+	}
+	else
+	echo "Error! Fields are empty!";
+				 }
+
+if(isset($_POST['submit']))
+	SignIn();
+
+?>
