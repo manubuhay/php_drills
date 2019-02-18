16d58b31c503516a3539f8c427dd8ddbd4fd2aea
Manu Buhay <manubuhay@gmail.com>
2019-02-18 15:34:41 +0800

    removed apostope

--- simple_login_page/connectivity.php
+++ simple_login_page/connectivity.php
@@ -13,7 +13,7 @@ $db=mysql_select_db(DB_NAME,$con) or die("Failed to connect to MySQL: " . mysql_
 function SignIn(){ 
 session_start(); //starting the session for user profile page
 
-	if(!empty($_POST['user']) AND !empty($_POST['pass']))
+	if(!empty($_POST[user]) AND !empty($_POST[pass]))
 	{
 		$query = mysql_query("SELECT * FROM UserName where userName = '$_POST[user]' AND pass = '$_POST[pass]'") or die(mysql_error());
 		$row = mysql_fetch_array($query) or die(mysql_error());
@@ -25,15 +25,15 @@ session_start(); //starting the session for user profile page
 				}
 	}
 
-	if(empty($_POST['pass']))
+	if(empty($_POST[pass]))
 		echo "Password is empty!";
 
-	if(empty($_POST['user']))
+	if(empty($_POST[user]))
 		echo "Username is empty!";
 
 	else
 	echo "Error! Fields are empty!";
 				 }
-if(isset($_POST['submit']))
+if(isset($_POST[submit]))
 	SignIn();
 ?>
