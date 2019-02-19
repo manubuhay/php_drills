8d09c2f09fc76909dc8bb35027ff3869a3e5997b
Manu Buhay <manubuhay@gmail.com>
2019-02-19 16:10:46 +0800

    replaced php variables with post variables

--- simple_login_page/connectivity.php
+++ simple_login_page/connectivity.php
@@ -7,15 +7,15 @@ define('DB_PASSWORD','password');
 $con=mysql_connect(DB_HOST,DB_USER,DB_PASSWORD) or die("Failed to connect to MySQL: " . mysql_error()); 
 $db=mysql_select_db(DB_NAME,$con) or die("Failed to connect to MySQL: " . mysql_error());
 
-$id = $_POST['user']; 
-$pw = $_POST['pass'];
+/*$id = $_POST['user']; 
+$pw = $_POST['pass'];*/
 
 function signIn(){ 
 session_start(); //starting the session for user profile page
 
-	if(!empty('$id') AND !empty('$pw'))
+	if(!empty($_POST[user]) AND !empty($_POST[pass]))
 	{
-		$query = mysql_query("SELECT * FROM UserName where userName = '$id' AND pass = '$pw'") or die(mysql_error());
+		$query = mysql_query("SELECT * FROM UserName where userName = '$_POST[user]' AND pass = '$_POST[pass]'") or die(mysql_error());
 		$row = mysql_fetch_array($query) or die(mysql_error());
 			if(!empty($row['userName']) AND !empty($row['pass']))
 				{
@@ -27,9 +27,9 @@ session_start(); //starting the session for user profile page
 
 	else
 	{
-		if(empty('$id'))
+		if(empty($_POST[user]))
 			echo "Empty Username!";
-		if(empty('$pw'))
+		if(empty($_POST[pass]))
 			echo "Empty Password!";
 /*	if(empty($_POST[pass]))
 		echo "Password is empty!";
