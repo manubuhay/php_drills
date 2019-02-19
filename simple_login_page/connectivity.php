dc108249fb52d6fecf966fd05639a39d2034cb5e
Manu Buhay <manubuhay@gmail.com>
2019-02-19 16:58:28 +0800

    removed echo user

--- simple_login_page/connectivity.php
+++ simple_login_page/connectivity.php
@@ -27,7 +27,6 @@ session_start(); //starting the session for user profile page
 			echo "Empty Username!";
 		if(empty($_POST[pass]))
 			echo "Empty Password!";
-			echo "$id";
 		else
 			echo "Error! Fields are empty!";
 	}			 }
