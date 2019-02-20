43e059f75fd41ba458407404268bf9c730ae3e8a
Manu Buhay <manubuhay@gmail.com>
2019-02-20 13:48:38 +0800

    moved else statement outside not empty row condition

--- simple_login_page/connectivity.php
+++ simple_login_page/connectivity.php
@@ -13,16 +13,15 @@ session_start(); //starting the session for user profile page
 
 	if(!empty($_POST[user]) and !empty($_POST[pass]))
 	{
-		$query = mysql_query("SELECT * FROM UserName where userName = '$_POST[user]' AND pass = '$_POST[pass]'") or die("Failed to connect to MySQL: " . mysql_error());
+		$query = mysql_query("SELECT * FROM UserName where userName = '$_POST[user]' AND pass = '$_POST[pass]'") or die(mysql_error());
 		$row = mysql_fetch_array($query) or die(mysql_error());
 			if(!empty($row['userName']) and !empty($row['pass']))
 				{
 					if($_POST[user] == $row['userName'] and $_POST[pass] == $row['pass'])
-
-						echo "Username does not exist. / Password does not match.";
-					else
 						header('Location: login.html');
 				}
+			else
+						echo "Username does not exist. / Password does not match.";	
 	}
 	else
 	echo "Error! Field/s are missing!";			 
