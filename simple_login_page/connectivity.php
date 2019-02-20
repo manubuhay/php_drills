5167d8190a5f409b3d0b60578e769825129d1af2
Manu Buhay <manubuhay@gmail.com>
2019-02-20 13:58:39 +0800

    added failed to connect in ftch array function

--- simple_login_page/connectivity.php
+++ simple_login_page/connectivity.php
@@ -13,15 +13,15 @@ session_start(); //starting the session for user profile page
 
 	if(!empty($_POST[user]) and !empty($_POST[pass]))
 	{
-		$query = mysql_query("SELECT * FROM UserName where userName = '$_POST[user]' AND pass = '$_POST[pass]'") or die(mysql_error());
-		$row = mysql_fetch_array($query) or die(mysql_error());
+		$query = mysql_query("SELECT * FROM UserName where userName = '$_POST[user]' and pass = '$_POST[pass]'") or die(mysql_error());
+		$row = mysql_fetch_array($query) or die("Failed to connect to MySQL: " . mysql_error());
 			if(!empty($row['userName']) and !empty($row['pass']))
 				{
 					if($_POST[user] == $row['userName'] and $_POST[pass] == $row['pass'])
 						header('Location: login.html');
 				}
 			else
-						echo "Username does not exist. / Password does not match.";	
+				echo "Username does not exist. / Password does not match.";	
 	}
 	else
 	echo "Error! Field/s are missing!";			 
