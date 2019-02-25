251720c7417dd9e5b64e68da79c8677862fb8763
Manu Buhay <manubuhay@gmail.com>
2019-02-25 13:54:09 +0800

    edit indentation

--- simple_login_page/connectivity.php
+++ simple_login_page/connectivity.php
@@ -10,20 +10,19 @@ $db=mysql_select_db(DB_NAME,$con) or die("Failed to connect to MySQL: " . mysql_
 function signIn()
  { 
 session_start(); //starting the session for user profile page
-
-	if(!empty($_POST[user]) and !empty($_POST[pass]))
-	{
-		$query = mysql_query("SELECT * FROM UserName where userName = '$_POST[user]' and pass = '$_POST[pass]'") or die(mysql_error());
-		$row = mysql_fetch_array($query) or die("Error, empty set! " . mysql_error());
-			if(!empty($row['userName']) and !empty($row['pass']))
-				{
-					if($_POST[user] == $row['userName'] and $_POST[pass] == $row['pass'])
-						header('Location: login.html');
-				}
-	}
-	else
-	echo "Error! Field/s are missing!";			 
- }
+ if(!empty($_POST[user]) and !empty($_POST[pass]))
+  {
+   $query = mysql_query("SELECT * FROM UserName where userName = '$_POST[user]' and pass = '$_POST[pass]'") or die(mysql_error());
+   $row = mysql_fetch_array($query) or die("Error, empty set! " . mysql_error());
+	if(!empty($row['userName']) and !empty($row['pass']))
+	 {
+	  if($_POST[user] == $row['userName'] and $_POST[pass] == $row['pass'])
+	  header('Location: login.html');
+	 }
+  }
+ else
+  echo "Error! Field/s are missing!";			 
+  }
 if(isset($_POST[submit]))
 	signIn();
 else
